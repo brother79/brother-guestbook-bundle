0dd6d55b501e016e23cde230080707b46f58847d
git <brother79@mail.ru>
2015-07-14 15:52:06 +0600

    guestbook

--- Controller/EntryController.php
+++ Controller/EntryController.php
@@ -5,6 +5,7 @@ namespace Brother\GuestbookBundle\Controller;
 use Brother\CommonBundle\AppTools;
 use Brother\CommonBundle\Controller\BaseController;
 use Brother\CommonBundle\Model\BaseApi;
+use Brother\CommonBundle\Model\Entry\EntryInterface;
 use Brother\GuestbookBundle\Form\EntryType;
 use Brother\GuestbookBundle\Model\EntryManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -37,6 +38,38 @@ class EntryController extends BaseController
         }
 
         return $this->manager;
+    }
+
+    /**
+     * Returns the requested Form Factory service
+     *
+     * @param string $name
+     *
+     * @return \Symfony\Component\Form\Form
+     */
+    public function getFormFactory($name)
+    {
+        return $this->container->get('brother_guestbook.form_factory.' . $name);
+    }
+
+    /**
+     * Returns the guestbook entry for a given id
+     *
+     * @param $id
+     *
+     * @return EntryInterface
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    public function getEntry($id)
+    {
+        $entry = $this->getManager()->findOneById($id);
+
+        if (null === $entry) {
+            throw new NotFoundHttpException(sprintf("Guestbook entry with id '%s' does not exists.", $id));
+        }
+
+        return $entry;
     }
 
     /**
@@ -379,60 +412,14 @@ class EntryController extends BaseController
         return $this->render($view, array('form' => $form->createView()));
     }
 
-
-
-    /**
-     * Returns the requested Form Factory service
-     *
-     * @param string $name
-     *
-     * @return \Symfony\Component\Form\Form
-     */
-    private function getFormFactory($name)
-    {
-        return $this->container->get('brother_guestbook.form_factory.' . $name);
-    }
-
-    /**
-     * Returns the requested View
-     *
-     * @param string $name
-     *
-     * @return string
-     */
-    public function getView($name)
-    {
-        return $this->container->getParameter('brother_guestbook.view.' . $name);
-    }
-
-    /**
-     * Returns the guestbook entry for a given id
-     *
-     * @param $id
-     *
-     * @return EntryInterface
-     *
-     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-     */
-    public function getEntry($id)
-    {
-        $guestbook = $this->getManager()->findOneById($id);
-
-        if (null === $guestbook) {
-            throw new NotFoundHttpException(sprintf("Guestbook entry with id '%s' does not exists.", $id));
-        }
-
-        return $guestbook;
-    }
-
     /**
      * Translate and set Flash bag message
      *
-     * @param string $msg
-     * @param array $args
-     * @param string $type
+     * @param string 	$msg
+     * @param array 	$args
+     * @param string 	$type
      */
-    public function setFlashMessage($msg, $args = array(), $type = 'notice')
+    public function setFlashMessage($msg, $args=array(), $type='notice')
     {
         $msg = $this->get('translator')->trans($msg, $args, 'BrotherGuestbookBundle');
         $this->get('session')->getFlashBag()->add($type, $msg);
@@ -444,10 +431,9 @@ class EntryController extends BaseController
      * @param string $msg
      * @param string $type
      */
-    public function setFlashBag($msg, $type = 'notice')
+    public function setFlashBag($msg, $type='notice')
     {
         $this->get('session')->getFlashBag()->add($type, $msg);
     }
-
 
 }
